2c1e3c4db61fab96ebe87f68e59b88259c0f8dfb
Sungtak Lee <taklee@google.com>
2022-09-14 06:18:55 +0000

    CCodec: Delay stop using surface until the end of release()
    Delay stop using surface until the end of release() in order not to
    interfere internal Codec work.
    
    Bug: 242685100
    Test: atest android.media.codec.cts.MediaCodecTest
    Test: atest android.media.decoder.cts.AdaptivePlaybackTest
    Change-Id: I9a4639d5cfd58feb91655713191e4e75bae06b24

--- media/codec2/sfplugin/CCodec.cpp
+++ media/codec2/sfplugin/CCodec.cpp
@@ -1992,6 +1992,7 @@ void CCodec::release(bool sendCallback) {
         comp = state->comp;
     }
     comp->release();
+    mChannel->stopUseOutputSurface();
 
     {
         Mutexed<State>::Locked state(mState);
